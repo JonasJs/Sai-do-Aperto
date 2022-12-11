9f833574ba6dce266d12ca5c1dd0eec1411eb5c6
Jonas Alves <joonasbalves@gmail.com>
2022-12-11 18:24:15 -0300

    config styled-component

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  white: "#ffffff",
+  
+  brand_500: "#516DF6",
+  
+  red_500: "#FD4342",
+
+  blue_900: "#072949",
+
+  gray_500: "#9DACBA",
+
+  yellow_500 : "#FFBE0B"
+}
